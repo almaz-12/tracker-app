447b609ed5334b978db539a011291a257f167fd4
Almaz Khabibullin <adreams2016@yandex.ru>
2025-10-15 07:52:47 +0300

    создание store медитаций, вывод медитаций

---
+++ src/components/MeditationItem.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { Meditation } from '@/interfaces/meditations.interfaces'
+
+const props = defineProps<Meditation>()
+</script>
+
+<template>
+  <div class="meditation-item">
+    {{ props.title }}
+    {{ props.description }}
+  </div>
+</template>
+
+<style></style>
