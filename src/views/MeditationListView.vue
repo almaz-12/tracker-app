4aa76891b17b10640b003399195d8ae8eaa2a1ee
Almaz Khabibullin <adreams2016@yandex.ru>
2025-10-17 09:48:49 +0300

    главная страница и страница статистики

--- src/views/MeditationListView.vue
+++ src/views/MeditationListView.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import MeditationList from '@/components/MeditationList.vue'
+import AppLayout from '@/layouts/AppLayout.vue'
 </script>
 
 <template>
-  <MeditationList />
+  <AppLayout>
+    <MeditationList />
+  </AppLayout>
 </template>
 
 <style></style>
