7919462fd6b4d09d956918ad5342ba7d40ec00b4
Almaz Khabibullin <adreams2016@yandex.ru>
2025-10-17 09:48:32 +0300

    404 компонент

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,4 +4,14 @@ export const routes = [
     name: 'main',
     component: () => import('@/views/MeditationListView.vue'),
   },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: () => import('@/views/MeditationStatisticsView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/NotFound.vue'),
+    name: 'error-404',
+  },
 ]
---
+++ src/views/NotFound.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+function redirectToMain() {
+  router.push({
+    name: 'main',
+  })
+}
+</script>
+
+<template>
+  <div class="not-found">
+    <h1 class="not-found__title">404</h1>
+    <AppButton @click="redirectToMain">На главную</AppButton>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  gap: 52px;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.not-found__title {
+  font-size: 154px;
+  font-weight: 700;
+}
+</style>
