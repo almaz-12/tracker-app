67776f4b589623c9ffdf6df8507efda7e6584a90
Almaz Khabibullin <adreams2016@yandex.ru>
2025-10-19 14:23:37 +0300

    фикс

---
+++ src/components/VoteItem.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { MOOD_LABELS } from '@/common/constants'
+import IconFocus from '@/icons/IconFocus.vue'
+import IconOk from '@/icons/IconOk.vue'
+import IconRelax from '@/icons/IconRelax.vue'
+import type { MoodType } from '@/common/constants'
+
+const { name } = defineProps<{ name: MoodType }>()
+
+const components = {
+  good: IconOk,
+  relax: IconRelax,
+  focus: IconFocus,
+  anxiously: IconFocus,
+}
+</script>
+
+<template>
+  <div class="vote__questions-img">
+    <component :is="components[name]" />
+  </div>
+  <div class="vote__questions-text">{{ MOOD_LABELS[name] }}</div>
+</template>
+
+<style scoped>
+.vote__questions-img {
+  width: 62px;
+  height: 65px;
+  margin: auto;
+  margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.9);
+  transition: all 0.3s ease-in-out;
+}
+</style>
---
+++ src/components/VoteList.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { MOOD_STATES } from '@/common/constants'
+import VoteItem from './VoteItem.vue'
+</script>
+
+<template>
+  <div class="vote">
+    <div class="vote__title">Как вы сегодня себя чувствуете?</div>
+    <div class="vote__questions">
+      <div class="vote__questions-item" v-for="state in MOOD_STATES">
+        <VoteItem :name="state" :key="state" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.vote {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: flex-start;
+}
+.vote__title {
+  font-size: 22px;
+  line-height: 100%;
+  color: rgba(255, 255, 255, 0.7);
+}
+
+.vote__questions {
+  display: flex;
+  gap: 20px;
+}
+.vote__questions-item {
+  text-align: center;
+  cursor: pointer;
+}
+
+.vote__questions-item:hover .vote__questions-img {
+  background: rgba(105, 176, 156, 1);
+}
+</style>
